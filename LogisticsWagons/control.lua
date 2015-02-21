60f76fbb326f3ea817001f51db6127e0591a773e
Johan Paulsson <gonz@badcode.net>
2015-02-20 16:19:31 -0800

    Release changes

--- LogisticsWagons/control.lua
+++ LogisticsWagons/control.lua
@@ -1,7 +1,7 @@
 -- Debug functions needs to be in global scope to be able to be called from the classes
 
 function debugLog(message)
-	if true then -- set for debug
+	if false then -- set for debug
 		local string = math.floor(game.tick / 60) .. " : " .. message
 		game.player.print(string)
 		print(string)
--- package.sh
+++ package.sh
@@ -8,11 +8,11 @@ SVERSIONTAG=${VERSIONTAG//./_}
 SVERSIONTAG=${VERSIONTAG//v/_}
 RVERSIONTAG=`echo $VERSIONTAG | sed -e "s/v//"`
 
-echo "Updating version number"
+echo "Updating version number to $RVERSIONTAG"
 sed -e "s/\(\"version\": \"\).*\(\",\)/\1$RVERSIONTAG\2/" $FOLDER/info.json > $FOLDER/info.json_temp
 mv $FOLDER/info.json_temp $FOLDER/info.json
 
-echo "Creating zip file"
+echo "Creating zip file: $FOLDER$SVERSIONTAG.zip"
 cp -rf $FOLDER $FOLDER$SVERSIONTAG
 zip -rq $FOLDER$SVERSIONTAG.zip $FOLDER$SVERSIONTAG -x "*/\.*"
 rm -rf $FOLDER$SVERSIONTAG
